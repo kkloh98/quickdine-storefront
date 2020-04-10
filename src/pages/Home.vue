4b1e6b77ed169d3dcfc691ae91eb9ed78fb3149c
kkloh98 <40446720+kkloh98@users.noreply.github.com>
2020-04-10 15:30:54 +0800

    added store image

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,22 +1,11 @@
 <template>
   <v-flex fill-height>
-    <v-carousel
-      height="25vh"
-      style="width: 100%;"
-      hide-delimiter-background
-      delimiter-icon="mdi-minus"
-      :show-arrows="false"
-      cycle
-    >
-      <v-carousel-item
-        v-for="(image, i) in images"
-        :key="i"
-        reverse-transition="fade-transition"
-        transition="fade-transition"
-      >
-        <v-img :src="image.url"></v-img>
-      </v-carousel-item>
-    </v-carousel>
+    <v-img
+      v-if="store"
+      :src="store.image_url ? store.image_url : 'https://tinyurl.com/tvttsft'"
+      max-height="25vh"
+      min-width="100%"
+    ></v-img>
 
     <div class="mx-3">
       <v-card-title class="font-weight-bold pl-0">{{ store ? store.name : '' }}</v-card-title>
@@ -26,19 +15,6 @@
           <v-icon size="12" class="mr-1">mdi-clock-outline</v-icon>Closes in 2hrs
         </v-chip>
       </v-row>
-
-      <!-- <v-row justify="space-around" class="mx-0 my-10">
-        <v-btn outlined fab color="red darken-1" :to="`/stores/${$route.params.store_id}/menu?type=0`">
-          <v-icon>mdi-food</v-icon>
-        </v-btn>
-        <v-btn outlined fab color="blue darken-1" :to="`/stores/${$route.params.store_id}/menu?type=1`">
-          <v-icon>mdi-glass-mug-variant</v-icon>
-        </v-btn>
-        <v-btn outlined fab color="deep-purple darken-2" :to="`/stores/${$route.params.store_id}/menu?type=2`">
-          <v-icon>mdi-muffin</v-icon>
-        </v-btn>
-      </v-row>-->
-
       <v-btn
         :to="`/stores/${this.$route.params.store_id}/menu`"
         class="my-5"
@@ -51,24 +27,6 @@
       >
         <v-icon left size="17" class="mr-4">mdi-book-open-outline</v-icon>Expore Our Menu
       </v-btn>
-      <!--
-      <div class="mb-5">
-        <v-card-subtitle class="pl-0 pb-1 font-weight-bold">WE THINK YOU'LL LIKE</v-card-subtitle>
-        <v-row class="ma-0">
-          <v-list style="max-height: 220px; display: flex; overflow-x: auto; overflow-y: hidden">
-            <PopularProductCard v-for="i in 5" :key="i" />
-          </v-list>
-        </v-row>
-      </div>
-
-      <div class="mb-5">
-        <v-card-subtitle class="pl-0 pb-1 font-weight-bold">RECOMMENDED PRODUCTS</v-card-subtitle>
-        <v-row class="ma-0">
-          <v-list style="max-height: 220px; display: flex; overflow-x: auto; overflow-y: hidden">
-            <PopularProductCard v-for="i in 5" :key="i" />
-          </v-list>
-        </v-row>
-      </div>-->
 
       <div class="mb-5" v-for="(collection, i) in collections" :key="i">
         <v-card-subtitle class="pl-0 pb-1 font-weight-bold">{{ collection.name }}</v-card-subtitle>
@@ -100,13 +58,6 @@ export default {
 
   data: () => ({
     store: null,
-    images: [
-      { id: 1, url: 'https://tinyurl.com/tvttsft' },
-      {
-        id: 2,
-        url: 'https://singlestroke.io/wp-content/uploads/2015/10/high-quality-food-stock-photos-thumbnail.jpg',
-      },
-    ],
     collections: [],
   }),
 
